cf3ad70805ba00ceee8df74ded1c5dafe32e8375
Tim Wright <spartas@gmail.com>
2013-08-09 14:46:17 -0500

    Initial Commit

---
+++ CoreDataApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataApp
+//
+//  Created by Timothy Wright on 8/9/13.
+//  Copyright (c) 2013 MAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
